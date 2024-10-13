082efdf2e1c3723991e25b480eb6c4cc71e7aba2
Sukhrob Abdullaev <s.abdullaev@tuit.uz>
2024-10-13 21:02:37 +0900

    add

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,7 +4,7 @@ export const token = localStorage.getItem("access_token");
 
 export const decodedToken = token ? jwtDecode(token) : null;
 
-export const BASE_URL = "http://45.55.194.72:8003/api";
+export const BASE_URL = "http://134.209.221.207:8003/api";
 
 export const typeletter_id = localStorage.getItem("template_id") || "1";
 
